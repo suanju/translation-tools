4dd31053beac74fc1e6ee25320d7f0e47e1ef573
橡皮擦 <2506152074@qq.com>
2023-09-03 16:51:45 +0800

    初始化前端项目

---
+++ web/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup>
+
+</script>
---
+++ web/nuxt.config.ts
@@ -0,0 +1,11 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  pages: true,
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
